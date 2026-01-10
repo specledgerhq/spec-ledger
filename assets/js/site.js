d117841a35f1e7d21ee0878a1fe37679b4331f58
specledgerhq <andrewsaistuff@gmail.com>
2026-01-10 22:06:20 +0000

    Rebuild homepage and base site structure

---
+++ assets/js/site.js
@@ -0,0 +1,18 @@
+// SpecLedger - Site JavaScript
+// Reserved for future enhancements
+
+(function() {
+  'use strict';
+
+  // Initialize site on page load
+  function init() {
+    // Future functionality can be added here
+  }
+
+  // Run initialization when DOM is ready
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
